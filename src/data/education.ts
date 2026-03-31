9113a55a0876b99ad162e7e6efc2b79ce1cfce08
Farid Rashidi <farid.rsh@gmail.com>
2026-03-30 20:58:22 -0400

    ✨ Update degree for National Institutes of Health education entry

--- src/data/education.ts
+++ src/data/education.ts
@@ -11,7 +11,7 @@ export type EducationEntry = {
 export const education: EducationEntry[] = [
   {
     institution: "National Institutes of Health",
-    degree: "Predoctoral Partnerships Program",
+    degree: "NIH Graduate Partnership Program (GPP)",
     href: "https://www.nih.gov/",
     logoScale: "1.15",
     logoSrc: "https://images.farid.one/logo/nih.svg",
