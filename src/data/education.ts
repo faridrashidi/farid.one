f3229b7ef30fdf301d6a252cf3c28653c9966e73
Farid Rashidi <farid.rsh@gmail.com>
2026-08-16 00:24:05 -0400

    🍱 Update logo asset URLs

--- src/data/education.ts
+++ src/data/education.ts
@@ -14,7 +14,7 @@ export const education: EducationEntry[] = [
     degree: "NIH Graduate Partnership Program (GPP)",
     href: "https://www.nih.gov/",
     logoScale: "1.15",
-    logoSrc: "https://cdn.farid.one/farid/nih.svg",
+    logoSrc: "https://cdn.farid.one/farid/logo_nih.svg",
   },
   {
     institution: "Indiana University",
@@ -22,20 +22,20 @@ export const education: EducationEntry[] = [
     href: "https://www.iu.edu/",
     logoOffsetY: "-3px",
     logoScale: "0.9",
-    logoSrc: "https://cdn.farid.one/farid/iu.svg",
+    logoSrc: "https://cdn.farid.one/farid/logo_iu.svg",
   },
   {
     institution: "Sharif University of Technology",
     degree: "M.S. in Artificial Intelligence and Robotics",
     href: "https://www.sharif.ir/",
     markPadding: "0.22rem",
-    logoSrc: "https://cdn.farid.one/farid/sut.svg",
+    logoSrc: "https://cdn.farid.one/farid/logo_sut.svg",
   },
   {
     institution: "Amirkabir University of Technology",
     degree: "B.S. in Software Engineering",
     href: "https://aut.ac.ir/",
     markPadding: "0.18rem",
-    logoSrc: "https://cdn.farid.one/farid/aut.svg",
+    logoSrc: "https://cdn.farid.one/farid/logo_aut.svg",
   },
 ];
--- src/data/sport.ts
+++ src/data/sport.ts
@@ -10,12 +10,12 @@ export const sportPhotos: SportPhotoEntry[] = [
     alt: "Farid performing a high kick in a white karate uniform by the sea",
     label: "Kish Island, Iran 2012",
     rotationClass: "sport-polaroid--left",
-    src: "https://cdn.farid.one/farid/high-kick.webp",
+    src: "https://cdn.farid.one/farid/karate_high-kick.webp",
   },
   {
     alt: "Two martial artists training by the sea, with Farid performing a jumping kick",
     label: "Kish Island, Iran 2012",
     rotationClass: "sport-polaroid--right",
-    src: "https://cdn.farid.one/farid/flying-side-kick.webp",
+    src: "https://cdn.farid.one/farid/karate_flying-side-kick.webp",
   },
 ];
