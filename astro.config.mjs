e74076014ba1fa3bc26abc8cb8d972b36d4d54c1
Farid Rashidi <farid.rsh@gmail.com>
2026-09-05 10:27:37 -0400

    ⚡️ Improve PageSpeed scores

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
@@ -8,6 +8,32 @@ export default defineConfig({
     collectionsBackwardsCompat: true,
   },
   site: "https://farid.one",
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Inter",
+      cssVariable: "--font-inter",
+      weights: ["100 900"],
+      subsets: ["latin"],
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Vazirmatn",
+      cssVariable: "--font-vazirmatn",
+      weights: ["100 900"],
+      styles: ["normal"],
+      subsets: ["arabic", "latin"],
+    },
+  ],
+  image: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.gravatar.com",
+        pathname: "/avatar/**",
+      },
+    ],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
