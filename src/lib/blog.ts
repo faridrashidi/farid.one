8f63acd4675669e6162f26b4ff1d31deed4819bd
Farid Rashidi <farid.rsh@gmail.com>
2026-09-10 15:33:52 -0400

    ♻️ Centralize blog post queries

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -1,2 +1,10 @@
+import { getCollection } from "astro:content";
+
+export const getSortedBlogPosts = async () =>
+  (await getCollection("blog")).sort(
+    (left, right) =>
+      right.data.publishedAt.getTime() - left.data.publishedAt.getTime(),
+  );
+
 export const getBlogEntryPath = (entryId: string) =>
   entryId.replace(/\.[^/.]+$/, "").replace(/\/index$/, "");
